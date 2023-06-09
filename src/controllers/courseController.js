7d89e000684187dfa42d4391b3aff317502ba4ef
divyaGrvgithub <divyamalatiwarimtech@gmail.com>
2023-06-09 15:22:13 +0530

    course model and course controller completed

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -1,21 +1,89 @@
 const Course = require('../models/course');
 
-exports.getAllCourses = (req, res) => {
-  // Implementation to fetch all courses
+const getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find();
+    return res.status(200).json(courses);
+  } catch (error) {
+    return res.status(500).json({ error: 'An error occurred while retrieving the courses' });
+  }
 };
 
-exports.getCourseById = (req, res) => {
-  // Implementation to fetch a course by ID
+const getCourseById = async (req, res) => {
+  try {
+    const courseId = req.params.id;
+    const course = await Course.findById(courseId);
+
+    if (!course) {
+      return res.status(404).json({ error: 'Course not found' });
+    }
+    return res.status(200).json(course);
+  } catch (error) {
+    return res.status(500).json({ error: 'An error occurred while retrieving the course' });
+  }
 };
 
-exports.createCourse = (req, res) => {
-  // Implementation to create a new course
+const createCourse = async (req, res) => {
+  try {
+    const { title, description, instructor, duration, price, lessons } = req.body;
+    const newCourse = new Course({
+      title,
+      description,
+      instructor,
+      duration,
+      price,
+      lessons,
+    });
+
+    await newCourse.save();
+    return res.status(201).json({ message: 'Course created successfully' });
+  } catch (error) {
+    return res.status(500).json({ error: 'An error occurred while creating the course' });
+  }
 };
 
-exports.updateCourse = (req, res) => {
-  // Implementation to update a course
+const updateCourse = async (req, res) => {
+  try {
+    const courseId = req.params.id;
+    const { title, description, instructor, duration, price, lessons } = req.body;
+
+    const course = await Course.findByIdAndUpdate(
+      courseId,
+      {
+        title,
+        description,
+        instructor,
+        duration,
+        price,
+        lessons,
+      },
+      { new: true }
+    );
+
+    if (!course) {
+      return res.status(404).json({ error: 'Course not found' });
+    }
+    return res.status(200).json({ message: 'Course updated successfully' });
+  } catch (error) {
+    return res.status(500).json({ error: 'An error occurred while updating the course' });
+  }
 };
 
-exports.deleteCourse = (req, res) => {
-  // Implementation to delete a course
+const deleteCourse = async (req, res) => {
+  try {
+    const courseId = req.params.id;
+
+    const course = await Course.findByIdAndDelete(courseId);
+
+    if (!course) {
+      return res.status(404).json({ error: 'Course not found' });
+    }
+
+    return res.status(200).json({ message: 'Course deleted successfully' });
+  } catch (error) {
+    return res.status(500).json({ error: 'An error occurred while deleting the course' });
+  }
 };
+
+module.exports={getAllCourses,getCourseById,createCourse,updateCourse,deleteCourse };
+
